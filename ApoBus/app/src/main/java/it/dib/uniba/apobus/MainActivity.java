38c6ab59356b953515a9b32650a41395e31dbfdf
monti <r.monti1@studenti.uniba.it>
2024-08-06 09:12:27 +0200

    Aggiunta schermata di Splash

--- ApoBus/app/src/main/java/it/dib/uniba/apobus/MainActivity.java
+++ ApoBus/app/src/main/java/it/dib/uniba/apobus/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
         View decorView = getWindow().getDecorView();       // otteniamo la view che rappresenta tutto ciò che la finestra contiene
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION; // flag che permette di impostare l'hide degli elementi di navigazione
         decorView.setSystemUiVisibility(uiOptions);
